cfa9ff3d2791b52f78a712f3bc61c1648aa7bd25
switzerchees <patrickmichel96@yahoo.de>
2024-02-16 08:40:29 +0100

    Change handlers to procedures

--- server/trpc/routers/index.ts
+++ server/trpc/routers/index.ts
@@ -1,5 +1,5 @@
 import { router } from '../trpc'
-import * as hello from '~/server/handlers/hello'
+import * as hello from '~/server/procedures/hello'
 
 export const appRouter = router({
   hello: router(hello),
