8253963a0690af6fb57c48e734e16fcc29d9aa3d
switzerchees <patrickmichel96@yahoo.de>
2024-02-21 22:00:28 +0100

    Testing schema update migration

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -4,6 +4,7 @@ export const users = pgTable('users', {
   id: serial('id').primaryKey(),
   name: text('name').notNull(),
   email: text('email').notNull().unique(),
+  username: text('username').notNull().unique(),
 })
 export const usersRelations = relations(users, ({ many }) => ({
   posts: many(posts),
