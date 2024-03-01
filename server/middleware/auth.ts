b2d01803cc02239bc7d537bab00d69e741041406
switzerchees <patrickmichel96@yahoo.de>
2024-03-01 09:36:14 +0100

    Fix getUserSession when startup to fast

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,6 +1,8 @@
 export default defineEventHandler(async (event) => {
   const { getUserSession } = useLucia()
-  const { user, session } = await getUserSession(event)
-  event.context.user = user
-  event.context.session = session
+  if (getUserSession) {
+    const { user, session } = await getUserSession(event)
+    event.context.user = user
+    event.context.session = session
+  }
 })
