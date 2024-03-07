4630b7fbd9bd9c5508df22cc5a1829633335301c
switzerchees <patrickmichel96@yahoo.de>
2024-03-07 08:27:18 +0100

    Put pino pretty into normal dependencies

--- server/utils/useLogger.ts
+++ server/utils/useLogger.ts
@@ -5,7 +5,16 @@ let _logger: pino.Logger
 const useLogger = () => {
   if (!_logger) {
     const { logLevel } = useEnv()
-    _logger = pino({ level: logLevel })
+
+    _logger = pino({
+      level: logLevel,
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+        },
+      },
+    })
   }
   return _logger
 }
