faf5c2cfa41452a0ed3ef91457d6f04b309be4c6
switzerchees <patrickmichel96@yahoo.de>
2024-03-16 20:24:38 +0100

    Fix showToggleAll

--- components/User/Roles.vue
+++ components/User/Roles.vue
@@ -6,6 +6,7 @@
       :options="roles"
       :placeholder="$t('selectRoles')"
       :option-disabled="isMyUserAndAdmin"
+      :show-toggle-all="false"
       class="w-full md:w-20rem">
       <template #option="{ option }">
         <span class="truncate">{{ $t(option.name) }}</span>
